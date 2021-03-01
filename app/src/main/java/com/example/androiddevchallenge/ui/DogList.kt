9c493d1e9a3c316a7d869c8044c8516e78790354
zak <linhenji@163.com>
2021-03-02 02:24:17 +0800

    fix: action bug fix and add licensed content

--- app/src/main/java/com/example/androiddevchallenge/ui/DogList.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/DogList.kt
@@ -2,9 +2,15 @@ package com.example.androiddevchallenge.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
